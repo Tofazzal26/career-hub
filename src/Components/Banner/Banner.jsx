8c3e5d2c47a8fb40fff69a274354ebb202fd2c08
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-23 18:45:30 +0600

    banner style finish

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,26 +2,32 @@ import user from "../../assets/images/user.png";
 
 const Banner = () => {
   return (
-    <div className="container mx-auto">
-      <div className="flex items-center">
-        <div>
-          <h1 className="text-[70px] font-extrabold">
-            One Step Closer To Your
-            <span className="bg-gradient-to-r from-[#7E90FE] to-[#9873FF] bg-clip-text text-transparent">
-              Dream Job
-            </span>
-          </h1>
-          <p className="text-lg font-medium text-[#757575]">
-            Explore thousands of job opportunities with all the information you
-            need. Its your future. Come find it. Manage all your job application
-            from start to finish.
-          </p>
-          <button className="btn text-lg text-white  bg-gradient-to-r from-[#7E90FE] to-[#9873FF] font-bold">
-            Get Started
-          </button>
-        </div>
-        <div>
-          <img className="h-[550px] w-[800px]" src={user} alt="" />
+    <div className="mt-4">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <div className="text-left">
+            <h1 className="text-[65px] leading-[80px] w-[570px] font-extrabold">
+              One Step Closer To Your{" "}
+              <span className="bg-gradient-to-r from-[#7E90FE] to-[#9873FF] bg-clip-text text-transparent">
+                Dream Job
+              </span>
+            </h1>
+            <p className="text-lg font-medium w-[500px] my-8 text-[#757575]">
+              Explore thousands of job opportunities with all the information
+              you need. Its your future. Come find it. Manage all your job
+              application from start to finish.
+            </p>
+            <button className="btn text-lg text-white  bg-gradient-to-r from-[#7E90FE] to-[#9873FF] font-bold">
+              Get Started
+            </button>
+          </div>
+          <div>
+            <img
+              className="h-[550px] w-[600px] border-2 border-dotted"
+              src={user}
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
