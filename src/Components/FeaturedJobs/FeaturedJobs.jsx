897f1f0647d24b095a0f1e9c670c22619c344646
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-23 21:23:57 +0600

    see all jobs validation complete

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,8 @@ import FeaturedJobCard from "../FeaturedJobCard/FeaturedJobCard";
 
 const FeaturedJobs = () => {
   const [features, setFeatures] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
+
   useEffect(() => {
     fetch("../../../public/jobs.json")
       .then((res) => res.json())
@@ -20,9 +22,21 @@ const FeaturedJobs = () => {
           </p>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-          {features.map((feature) => (
+          {features.slice(0, dataLength).map((feature) => (
             <FeaturedJobCard key={feature.id} feature={feature} />
           ))}
+        </div>
+        <div
+          className={
+            features.length === dataLength ? `hidden` : `text-center mt-8`
+          }
+        >
+          <button
+            onClick={() => setDataLength(features.length)}
+            className="text-lg text-white py-3 px-4 rounded-md bg-gradient-to-r from-[#7E90FE] to-[#9873FF] font-bold"
+          >
+            See All Jobs
+          </button>
         </div>
       </div>
     </div>
