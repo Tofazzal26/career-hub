b2e133fdfea91f0e69d6503f14a28977ea145510
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-24 10:10:22 +0600

    applied sort validation finish

--- src/Components/AppliedJob/AppliedJob.jsx
+++ src/Components/AppliedJob/AppliedJob.jsx
@@ -7,6 +7,23 @@ const AppliedJob = () => {
   const jobs = useLoaderData();
 
   const [appliedJob, setAppliedJob] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJob);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJob.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJob.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJobs);
+    }
+  };
 
   useEffect(() => {
     const storedJob = getStoredApplication();
@@ -20,13 +37,30 @@ const AppliedJob = () => {
         }
       }
       setAppliedJob(jobsApplied);
+      setDisplayJobs(jobsApplied);
     }
-  }, []);
+  }, [jobs]);
 
   return (
     <div className="container mx-auto">
-      <div className="space-y-4">
-        {appliedJob.map((job) => (
+      <div className="space-y-4 mt-24 mb-24">
+        <div className="text-right">
+          <details className="dropdown">
+            <summary className="m-1 btn">Filter By</summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+              <li onClick={() => handleJobFilter("all")}>
+                <a>All</a>
+              </li>
+              <li onClick={() => handleJobFilter("remote")}>
+                <a>Remote</a>
+              </li>
+              <li onClick={() => handleJobFilter("onsite")}>
+                <a>Onsite</a>
+              </li>
+            </ul>
+          </details>
+        </div>
+        {displayJobs.map((job) => (
           <Applied key={job.id} job={job} />
         ))}
       </div>
